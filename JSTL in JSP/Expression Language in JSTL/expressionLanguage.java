3c5e6cda3464582d49738ea4fc11956c33b8a698
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-13 19:47:47 +0530

    Update expressionLanguage.java

--- JSTL in JSP/Expression Language in JSTL/expressionLanguage.java
+++ JSTL in JSP/Expression Language in JSTL/expressionLanguage.java
@@ -8,15 +8,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/exp");
-public class exp extends HttpServlet {
+@WebServlet("/expressionLanguage");
+public class expressionLanguage extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = "Ebaad Azam";
 		
 		request.setAttribute("label", name);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("lang.jsp"); //calling a JSP File from the Servlet Java Class
+		RequestDispatcher rd = request.getRequestDispatcher("expressionLanguage.jsp"); //calling a JSP File from the Servlet Java Class
 		rd.forward(request, response);
 				
 	}
