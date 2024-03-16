1cf3927c08b6dfc4f5c9b085919dd2e215ff8afa
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-16 14:46:34 +0530

    Update servlet.java

--- JSTL in JSP/Send Data from Servlet to JSP in Object Format/servlet.java
+++ JSTL in JSP/Send Data from Servlet to JSP in Object Format/servlet.java
@@ -1,1 +1,30 @@
+package com.ebaad;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/servlet")
+public class servlet extends HttpServlet {
+
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	System.out.println("Servlet executed");
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Ebaad"));
+        list.add(new Student(2, "Sarim"));
+        list.add(new Student(3, "Saad"));
+        list.add(new Student(4, "Atif"));
+
+        request.setAttribute("studs", list);
+        RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
+        rd.forward(request, response);
+    }
+}
 
