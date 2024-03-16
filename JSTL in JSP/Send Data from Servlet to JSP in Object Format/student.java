b3f8953a2f4364b9cb9b4c26335c4f0a81ea2cdb
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-03-16 14:45:55 +0530

    Update student.java

--- JSTL in JSP/Send Data from Servlet to JSP in Object Format/student.java
+++ JSTL in JSP/Send Data from Servlet to JSP in Object Format/student.java
@@ -1,1 +1,37 @@
+package com.ebaad;
+
+public class Student {
+	
+	int rollNo;
+	String name;
+	
+	public Student(int rollNo, String name) {
+		this.rollNo = rollNo;
+		this.name = name;
+	}
+	
+	// Setters and getters for rollNo and name
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// toString() method for string representation
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + "]";
+	}
+
+}
 
